74c05a21229163f27250687102c5a7b7158e3d6d
Raj Dhamdhere <rajdhamdhere2112@gmail.com>
2024-11-13 17:59:12 +0000

    removed portfolio toggler

--- client/src/Components/Test.js
+++ client/src/Components/Test.js
@@ -125,16 +125,7 @@ const Test = () => {
 						<h2>Portfolio</h2>
 						<p>Check our Portfolio</p>
 					</div>
-					<div className="row" data-aos="fade-up" data-aos-delay="100">
-						<div className="col-lg-12 d-flex justify-content-center">
-							<ul id="portfolio-flters">
-								<li data-filter="*" className="filter-active">All</li>
-								<li data-filter=".filter-app">App</li>
-								<li data-filter=".filter-card">Card</li>
-								<li data-filter=".filter-web">Web</li>
-							</ul>
-						</div>
-					</div>
+
 					<div className="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
 						{[
 							{ src: "assets/img/portfolio/portfolio-1.jpg", title: "App 1", type: "App" },

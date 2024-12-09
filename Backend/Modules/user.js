a53deb1a8a489bbac6f20aea153ae6984eae4c42
Raj Dhamdhere <rajdhamdhere2112@gmail.com>
2024-12-09 11:56:16 +0000

    Secure Channel for user.js and Appointment.js

--- Backend/Modules/user.js
+++ Backend/Modules/user.js
@@ -10,7 +10,7 @@ const encryptionKey = "Rajdhamdhere-is-Migate-Goku-akir";
 // Function to encrypt data
 function encryptData(plaintext) {
   const iv = crypto.randomBytes(16); // Generate a new Initialization Vector (IV) for each encryption
-  const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, iv);
+  const cipher = crypto.createCipheriv(process.env.DB_EncryptTechnique, encryptionKey, iv);
   let encrypted = cipher.update(plaintext, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return `${iv.toString('hex')}:${encrypted}`;
@@ -20,7 +20,7 @@ function encryptData(plaintext) {
 function decryptData(ciphertext) {
   const [ivHex, encrypted] = ciphertext.split(':');
   const iv = Buffer.from(ivHex, 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, iv);
+  const decipher = crypto.createDecipheriv(process.env.DB_EncryptTechnique, encryptionKey, iv);
   let decrypted = decipher.update(encrypted, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;

4d97b3aa93763897fbd95ba9d6d14d85e687c424
Raj Dhamdhere <rajdhamdhere2112@gmail.com>
2024-12-11 08:30:07 +0000

    Update Register

--- client/src/Components/RegisterForm.js
+++ client/src/Components/RegisterForm.js
@@ -245,7 +245,7 @@ const Register = () => {
               <div className="php-email-form mt-4" style={styles.formContainer}>
                 <div className="row">
                   <div className="col-md-6 form-group">
-                    <Form.Label style={{ fontWeight: "bold" ,color:"blue" }}>
+                    <Form.Label style={{ fontWeight: "bold"}}>
                       First Name
                     </Form.Label>
                     <input

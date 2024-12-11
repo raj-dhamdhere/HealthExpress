956898b727376a1957bcae8f77e0b55e5deb20d4
Raj Dhamdhere <rajdhamdhere2112@gmail.com>
2024-12-11 08:14:48 +0000

    Update Register Test

--- client/src/Components/RegisterForm.js
+++ client/src/Components/RegisterForm.js
@@ -246,7 +246,7 @@ const Register = () => {
                 <div className="row">
                   <div className="col-md-6 form-group">
                     <Form.Label style={{ fontWeight: "bold" }}>
-                      FULL Name
+                      First Name
                     </Form.Label>
                     <input
                       type="text"
